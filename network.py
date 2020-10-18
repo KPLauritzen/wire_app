4189c60b6cbaa432db818577d70863ce74ce5777
Kasper Primdal Lauritzen <KPLauritzen@gmail.com>
2020-10-18 16:30:10 +0200

    fix multigraph

--- network.py
+++ network.py
@@ -7,16 +7,17 @@ def load_graph():
         G = pickle.load(f)
     return G
 
-def get_connection(G, root, target, max_collabs_print=5):
+def get_connection(G, root, target, max_collabs_print=5, is_multigraph=False):
     path = nx.algorithms.shortest_path(G, root, target)
     path_str = " -> ".join(path)
     print(path_str)
-    print("---\nDetails:")
-    for start, end in zip(path, path[1:]):
-        collabs = G.get_edge_data(start, end)
-        collabs_str = ", ".join([work["work"] for work in collabs.values()])
-        if len(collabs) > max_collabs_print:
-            collabs_str += "..."
-        print(f"{start} -> {end}: {len(collabs)} collaboration(s)")
-        print(collabs_str)
+    if is_multigraph:
+        print("---\nDetails:")
+        for start, end in zip(path, path[1:]):
+            collabs = G.get_edge_data(start, end)
+            collabs_str = ", ".join([work["work"] for work in collabs.values()])
+            if len(collabs) > max_collabs_print:
+                collabs_str += "..."
+            print(f"{start} -> {end}: {len(collabs)} collaboration(s)")
+            print(collabs_str)
     return path_str
