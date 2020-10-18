72d607b3e7d9371ca62227da33e85086c4751c99
Kasper Primdal Lauritzen <KPLauritzen@gmail.com>
2020-10-18 13:31:09 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import network as n
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+G = n.load_graph()
+
+@app.route("/connection/<root>/<target>")
+def connection(root, target):
+    return n.get_connection(G, root, target)
+
+if __name__ == "__main__":
+    app.run()
---
+++ network.py
@@ -0,0 +1,22 @@
+import pickle
+
+import networkx as nx
+
+def load_graph():
+    with open("movie_graph.pkl", "rb") as f:
+        G = pickle.load(f)
+    return G
+
+def get_connection(G, root, target, max_collabs_print=5):
+    path = nx.algorithms.shortest_path(G, root, target)
+    path_str = " -> ".join(path)
+    print(path_str)
+    print("---\nDetails:")
+    for start, end in zip(path, path[1:]):
+        collabs = G.get_edge_data(start, end)
+        collabs_str = ", ".join([work["work"] for work in collabs.values()])
+        if len(collabs) > max_collabs_print:
+            collabs_str += "..."
+        print(f"{start} -> {end}: {len(collabs)} collaboration(s)")
+        print(collabs_str)
+    return path_str
